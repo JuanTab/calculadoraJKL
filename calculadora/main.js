16a600dc0ab2a756bd5a4c616c5b5022f7855674
JuanTa <136990866+JuanTab@users.noreply.github.com>
2024-03-25 02:59:53 -0500

    1.3
    correccion de errores

--- calculadora/main.js
+++ calculadora/main.js
@@ -3,9 +3,14 @@
 const pantalla = document.getElementById('pantalla');
 const botones = document.querySelectorAll(".btn");
 
+
 botones.forEach(boton => {
+    result = 0;
+    gnum = 0;
+    entr = 0;
         boton.addEventListener("click",()=> {
         const botonUndido = boton.textContent;
+        
         
         if(boton.id === "vaciar"){
             pantalla.textContent = "0";
@@ -21,21 +26,87 @@ botones.forEach(boton => {
 
         if(boton.id === "res"){
 
+                if (result >= 18){
+                    try {
+                        pantalla.textContent = eval(gnum + pantalla.textContent.slice(3));
+                        pantalla.textContent = eval(pantalla.textContent)
+                       
+                        result = pantalla.textContent.length;
+        
+                        if(result >= 18){
+                            gnum = pantalla.textContent;
+                            entr = result;
+                        }else {
+                            entr = 0;
+                            gnum = 0;}
+                        
+                    } catch {
+                        pantalla.textContent = "ERROR!";
+                    }  return; 
+                   
+                } else {
+                    try {
+               
+                        pantalla.textContent = eval(pantalla.textContent)
+                       
+                        result = pantalla.textContent.length;
+        
+                        if(result >= 18){
+                            gnum = pantalla.textContent;
+                            entr = result;
+                        }else 
+                        {
+                            entr = 0;
+                            gnum = 0;
+                        }
+                    } catch {
+                        pantalla.textContent = "ERROR!";
+                    }  return; 
+                }
+
 //en caso de que el usuario coloque operaciones sin sentido, este sera el mensaje que le saldra
-            try {
-                pantalla.textContent = eval(pantalla.textContent); 
-            } catch {
-                pantalla.textContent = "ERROR!";
-            }
-            return;
+            
+            
         }
+
+        
        
         if (pantalla.textContent === "0" || pantalla.textContent === "ERROR!"){
             pantalla.textContent = botonUndido;
         } else {
-            pantalla.textContent += botonUndido;
-        }
+//en caso de el numero tenga digitos mayores que el espacio que se tiene se ejecutara este bloque lo que convertiria el numero anterior en ans 
+//y el numero queda guardado en 
+            if (entr >= 18 ){
+                switch (botonUndido){
+                    case "+":  
+                        pantalla.textContent = "ans";
+                        pantalla.textContent += botonUndido;
+                    break;
 
+                    case "-":
+                        pantalla.textContent = "ans";
+                        pantalla.textContent += botonUndido;
+                        break;
+
+                    case "*":
+                        pantalla.textContent = "ans";
+                        pantalla.textContent += botonUndido;
+                        break;
+
+                    case "/":
+                        pantalla.textContent = "ans";
+                        pantalla.textContent += botonUndido;
+                        break;
+                    default:
+                        pantalla.textContent += botonUndido;
+                }
+                
+            } else {
+                pantalla.textContent += botonUndido;
+                
+                ;}
+        
+        }
     })
 })
 
