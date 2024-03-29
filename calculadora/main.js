a9b08c691914956304d813b609e6a843b0e120ec
JuanTa <136990866+JuanTab@users.noreply.github.com>
2024-03-28 21:38:41 -0500

    calculadoraV1.2
    en esta version de la calculadora se modificaron algunos errores y ademas se cambio el aspecto de esta, y se le agregaron los botones ()

--- calculadora/main.js
+++ calculadora/main.js
@@ -4,29 +4,46 @@ const pantalla = document.getElementById('pantalla');
 const botones = document.querySelectorAll(".btn");
 
 
+
 botones.forEach(boton => {
     result = 0;
     gnum = 0;
     entr = 0;
+
         boton.addEventListener("click",()=> {
         const botonUndido = boton.textContent;
         
         
         if(boton.id === "vaciar"){
             pantalla.textContent = "0";
+            result = "0";
+            gnum = "0";
+            entr = "0";
             return
         }
 
         if(boton.id === "eliminar"){
             if(pantalla.textContent.length === 1 || pantalla.textContent === "ERROR!"){
                 pantalla.textContent = "0";
-            } else {pantalla.textContent = pantalla.textContent.slice(0,-1);}
+            
+            } else {
+
+                pantalla.textContent = pantalla.textContent.slice(0,-1);
+             
+                if (pantalla.textContent.length === 3 && pantalla.textContent === "ans") {
+                    pantalla.textContent = gnum;
+                    entr = pantalla.textContent.length;
+                } else if (!(pantalla.textContent.length !== 3 && pantalla.textContent.substring(0,3) === "ans")){
+                    entr = pantalla.textContent.length;
+                    gnum = pantalla.textContent;
+                } return 
+            }
             return;
         }
 
         if(boton.id === "res"){
 
-                if (result >= 18){
+                if (result >= 17){
                     try {
                         pantalla.textContent = eval(gnum + pantalla.textContent.slice(3));
 
@@ -38,7 +55,7 @@ botones.forEach(boton => {
                            
                             result = pantalla.textContent.length;
             
-                            if(result >= 18){
+                            if(result >= 17){
                                 gnum = pantalla.textContent;
                                 entr = result;
                             }else {
@@ -60,7 +77,7 @@ botones.forEach(boton => {
                        
                             result = pantalla.textContent.length;
         
-                            if(result >= 18){
+                            if(result >= 17){
                                 gnum = pantalla.textContent;
                                 entr = result;
                             }else 
@@ -109,42 +126,51 @@ botones.forEach(boton => {
         } else {
 //en caso de el numero tenga digitos mayores que el espacio que se tiene se ejecutara este bloque lo que convertiria el numero anterior en ans 
 //y el numero queda guardado en 
-            if (entr >= 18 ){
+            if (entr >= 17 ){
                 switch (botonUndido){
                     case "+":  
                         pantalla.textContent = "ans";
                         pantalla.textContent += botonUndido;
-                        entr = pantalla.textContent;
+                        entr = 0;
                     break;
 
                     case "-":
                         pantalla.textContent = "ans";
                         pantalla.textContent += botonUndido;
-                        entr = pantalla.textContent;
+                        entr = 0;
                         break;
 
                     case "*":
                         pantalla.textContent = "ans";
                         pantalla.textContent += botonUndido;
-                        entr = pantalla.textContent;
+                        entr = 0;
                         break;
 
                     case "/":
                         pantalla.textContent = "ans";
                         pantalla.textContent += botonUndido;
-                        entr = pantalla.textContent;
+                        entr = 0;
                         break;
+
                     default:
                         pantalla.textContent += botonUndido;
                 }
                 
             } else {
-                pantalla.textContent += botonUndido;
-                ;}
+           
+                    let pnt = pantalla.textContent;
+                    let final = pnt.length - 1;
+                    
+                if (botonUndido === "(" && !(pnt.lastIndexOf("*") === final || pnt.lastIndexOf("/") === final || pnt.lastIndexOf("+") === final || pnt.lastIndexOf("-") === final) )
+                {
+                    pantalla.textContent += "*("
+                } else if(botonUndido !== ("+"||"*"||"-"||"/") && pnt.lastIndexOf(")") === final ){
+                    pantalla.textContent += "*";
+                    pantalla.textContent += botonUndido;
+                } 
+                else pantalla.textContent += botonUndido;
+                }
         
         }
     })
 })
-
-
-
